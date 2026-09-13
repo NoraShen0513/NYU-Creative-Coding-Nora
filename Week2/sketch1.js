89fac02a27d4b4606835eab7e28f4d60949cb61b
NoraShen0513 <nora_shen@126.com>
2026-09-13 00:03:56 -0400

    finish sketch 1

--- Week2/sketch1.js
+++ Week2/sketch1.js
@@ -6,17 +6,26 @@ function setup(){
     background(0);
     colorMode(HSB);
     //rectColor = color(random(185, 255), random(80, 100), random(90, 100));
+    frameRate(2);
 }
 
 
 function draw(){
     for (let x = 0; x < width; x += 100) {
         for (let y = 0; y < height; y += 100) {
-            rectColor = color(random(185, 255), random(80, 100), random(90, 100));
+            rectColor = color(random(190, 230), random(0, 100), random(90, 100));
             fill(rectColor);
             noStroke();
             rect(x,y,100,100);
         }
     }
     noLoop();
+}
+
+//function mouseMoved(){
+    //redraw();
+//}
+
+function mouseClicked() {
+  redraw();
 }
